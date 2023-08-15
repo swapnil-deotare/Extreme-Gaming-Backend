3a16918cb1309cd3f61fe39b5fc55a75ce287552
swapnil-deotare <swapnildeotare@gmail.com>
2023-08-14 23:27:29 -0700

    taking shape, will add dynamodb tomorrow. night

--- extremegaming/src/main/java/com/EG/extremegaming/player/PlayerController.java
+++ extremegaming/src/main/java/com/EG/extremegaming/player/PlayerController.java
@@ -1,5 +1,13 @@
 package com.EG.extremegaming.player;
 
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
 public class PlayerController {
 
+    @GetMapping("/player")
+    public String getPlayer() {
+        return "Player has been found";
+    }
 }
