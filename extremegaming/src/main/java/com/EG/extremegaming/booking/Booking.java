a77245d7a98976bf27b332950970fe0600ebecad
swapnil-deotare <swapnildeotare@gmail.com>
2023-08-14 20:24:01 -0700

    boiler chicken plate

--- extremegaming/src/main/java/com/EG/extremegaming/booking/Booking.java
+++ extremegaming/src/main/java/com/EG/extremegaming/booking/Booking.java
@@ -1,5 +1,73 @@
 package com.EG.extremegaming.booking;
 
+import java.util.Date;
+import java.util.List;
+
 public class Booking {
+
+    private String bookingId;
+    private String personName;
+    private List<String> friends;
+    private Date startTime;
+    private Date endTime;
+    private String type;
+
+    public Booking(String bookingId, String personName, List<String> friends, Date startTime, Date endTime,
+            String type) {
+        this.bookingId = bookingId;
+        this.personName = personName;
+        this.friends = friends;
+        this.startTime = startTime;
+        this.endTime = endTime;
+        this.type = type;
+    }
+
+    public String getBookingId() {
+        return bookingId;
+    }
+
+    public void setBookingId(String bookingId) {
+        this.bookingId = bookingId;
+    }
+
+    public String getPersonName() {
+        return personName;
+    }
+
+    public void setPersonName(String personName) {
+        this.personName = personName;
+    }
+
+    public List<String> getFriends() {
+        return friends;
+    }
+
+    public void setFriends(List<String> friends) {
+        this.friends = friends;
+    }
+
+    public Date getStartTime() {
+        return startTime;
+    }
+
+    public void setStartTime(Date startTime) {
+        this.startTime = startTime;
+    }
+
+    public Date getEndTime() {
+        return endTime;
+    }
+
+    public void setEndTime(Date endTime) {
+        this.endTime = endTime;
+    }
+
+    public String getType() {
+        return type;
+    }
+
+    public void setType(String type) {
+        this.type = type;
+    }
 
 }
--- extremegaming/src/main/java/com/EG/extremegaming/player/Player.java
+++ extremegaming/src/main/java/com/EG/extremegaming/player/Player.java
@@ -1,5 +1,60 @@
 package com.EG.extremegaming.player;
 
+import java.util.List;
+
 public class Player {
 
+    private String personId;
+    private String name;
+    private String phone;
+    private float hoursPlayed;
+    private List<String> friends;
+
+    public Player(String personId, String name, String phone, float hoursPlayed, List<String> friends) {
+        this.personId = personId;
+        this.name = name;
+        this.phone = phone;
+        this.hoursPlayed = hoursPlayed;
+        this.friends = friends;
+    }
+
+    public String getPersonId() {
+        return personId;
+    }
+
+    public void setPersonId(String personId) {
+        this.personId = personId;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getPhone() {
+        return phone;
+    }
+
+    public void setPhone(String phone) {
+        this.phone = phone;
+    }
+
+    public float getHoursPlayed() {
+        return hoursPlayed;
+    }
+
+    public void setHoursPlayed(float hoursPlayed) {
+        this.hoursPlayed = hoursPlayed;
+    }
+
+    public List<String> getFriends() {
+        return friends;
+    }
+
+    public void setFriends(List<String> friends) {
+        this.friends = friends;
+    }
 }
